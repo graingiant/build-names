e3cf0e0833ca22713829866142d0cbcd155d3dda
Alex Caza <alex@alexcaza.com>
2022-03-12 20:32:19 +0000

    Update release scripte

--- release.sh
+++ release.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-LATEST_TAG=git describe --tags $(git rev-list --tags --max-count=1)
-tar -cxvf  build-names-$TAG target/default/build-names
+LATEST_TAG=`git describe --tags $(git rev-list --tags --max-count=1)`
+tar -czvf  build-names-$LATEST_TAG.tar.gz target/default/build-names
