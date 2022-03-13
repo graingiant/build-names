b31baa90b62e73cb74d691bef139e7754642365b
Alex Caza <alex@alexcaza.com>
2022-03-13 14:10:32 +0000

    Create basic install script

---
+++ scripts/install.sh
@@ -0,0 +1,5 @@
+wget https://github.com/graingiant/build-names/releases/download/0.0.1/build-names-0.0.1.tar.gz
+tar -xvzf build-names-0.0.1.tar.gz
+sudo mv build-names /usr/local/bin/
+rm -rf build-names-0.0.1.tar.gz
+sudo chmod +x /usr/local/bin/build-names
--- scripts/release.sh
+++ scripts/release.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 LATEST_TAG=`git describe --tags $(git rev-list --tags --max-count=1)`
-tar -czvf  build-names-$LATEST_TAG.tar.gz target/default/build-names
+tar -czvf  build-names-$LATEST_TAG.tar.gz -C ../target/default/ build-names
